7d0848b483557a16eecb7a052e3485a228095db6
Vishal Sahare <vishal.sahare@gmail.com>
2026-07-26 13:42:05 +0530

    structured output

--- llmSearchTavilyBuiltInSearch.py
+++ llmSearchTavilyBuiltInSearch.py
@@ -1,3 +1,6 @@
+from typing import List
+from pydantic import BaseModel, Field
+
 from dotenv import load_dotenv
 from langchain.agents import create_agent
 from langchain.tools import tool
@@ -7,11 +10,21 @@ from langchain_tavily import TavilySearch
 
 load_dotenv(override=True)
 
+class Source(BaseModel):
+    """Schema of the source used by Agents"""
+
+    url: str = Field(description="URL of the source")
+
+class AgentResponse(BaseModel):
+    """Schema of the agent response with answer and sources"""
+
+    answer: str = Field(description="Answer to the user's query")
+    sources: List[Source] = Field(default_factory=list, description="List of sources used to answer the query")
 
 llm = ChatOpenAI(model="gpt-3.5-turbo")
 tools = [TavilySearch()]
 
-agent = create_agent(llm, tools)
+agent = create_agent(model=llm, tools=tools, response_format=AgentResponse)
 
 
 def main():
