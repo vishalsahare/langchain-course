eca8cf0a4a5986c5a3691c5aabe700cd8121cf2a
Vishal Sahare <vsahare@cisco.com>
2026-08-18 01:48:52 +0530

    setup graph node and chains

--- graph/nodes/src/nodes/__init__.py
+++ graph/nodes/src/nodes/__init__.py
@@ -1,2 +1,2 @@
-def main() -> None:
-    print("Hello from nodes!")
+def hello() -> str:
+    return "Hello from nodes!"
--- graph/src/graph/__init__.py
+++ graph/src/graph/__init__.py
@@ -1,2 +1,2 @@
-def main() -> None:
-    print("Hello from graph!")
+def hello() -> str:
+    return "Hello from graph!"
